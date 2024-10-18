1fea1c3c7fce0a55e2bc90f37dd2d4e01fba91df
Dan13l06 <167872814+Dan13l06@users.noreply.github.com>
2024-10-18 09:36:09 +0200

    Added a small part of the feature class

--- autoop/core/ml/feature.py
+++ autoop/core/ml/feature.py
@@ -5,8 +5,12 @@ import numpy as np
 
 from autoop.core.ml.dataset import Dataset
 
+
 class Feature(BaseModel):
-    # attributes here
+    """A description"""
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
 
     def __str__(self):
         raise NotImplementedError("To be implemented.")
