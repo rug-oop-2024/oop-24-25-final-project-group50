66906594822219fbae406371d4b894f538dc9b5f
njek02 <n.schepers@student.rug.nl>
2024-11-02 11:57:50 +0100

    Added to_artifact method

--- autoop/core/ml/model/model.py
+++ autoop/core/ml/model/model.py
@@ -1,25 +1,43 @@
 
-from abc import abstractmethod
-from autoop.core.ml.artifact import Artifact
-import numpy as np
-from copy import deepcopy
-from typing import Literal
+# from abc import abstractmethod
+# from autoop.core.ml.artifact import Artifact
+# import numpy as np
+# from copy import deepcopy
+# from typing import Literal
 
-class Model:
-    pass  # your code (attribute and methods) here
+# class Model:
+#     pass  # your code (attribute and methods) here
     
 
 from abc import ABC, abstractmethod
 from copy import deepcopy
+from autoop.core.ml.artifact import Artifact
 
 import numpy as np
 from pydantic import BaseModel, PrivateAttr
+
+"""
+Model still needs:
+    type attribute
+    to_artifact method
+
+    
+Implemented models:
+    MultipleLinearRegression - regression
+    KNN - classification
+    Lasso - regression
+    Random Forests - classification
+    Elastic Net - regression
+"""
+
+
 
 
 class Model(ABC, BaseModel):
     """An abstract base class for a generic Machine Learning model."""
 
     _parameters: dict = PrivateAttr(default={})
+    _type: str = PrivateAttr(default=None)
 
     @property
     def parameters(self) -> dict:
@@ -32,6 +50,18 @@ class Model(ABC, BaseModel):
             A deepcopy of the parameters
         """
         return deepcopy(self._parameters)
+    
+    @property
+    def type(self) -> str:
+        """
+        Get the type of the model using the getter.
+
+        Args:
+            None
+        Returns:
+            The model type
+        """
+        return self._type
 
     @abstractmethod
     def fit(self, observations: np.ndarray, ground_truth: np.ndarray) -> None:
@@ -58,3 +88,6 @@ class Model(ABC, BaseModel):
             An array containing n predictions based on the fitted data
         """
         pass
+
+    def to_artifact(self, name: str) -> Artifact:
+        return Artifact(name=name, type=self._type)
