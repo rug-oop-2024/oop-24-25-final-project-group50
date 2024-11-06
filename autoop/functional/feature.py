daea9f2a4b42d66faea9a0daa83e149a54b979dc
Daniël Prins <d.n.prins@student.rug.nl>
2024-11-06 13:24:17 +0100

    tried fixing binary string issues

--- autoop/functional/feature.py
+++ autoop/functional/feature.py
@@ -4,7 +4,7 @@ from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.feature import Feature
 import pandas as pd
 from pandas.api.types import is_numeric_dtype, is_object_dtype
-
+from io import StringIO
 
 def detect_feature_types(dataset: Dataset) -> List[Feature]:
     """Assumption: only categorical and numerical features and no NaN values.
@@ -14,12 +14,19 @@ def detect_feature_types(dataset: Dataset) -> List[Feature]:
         List[Feature]: List of features with their types.
     """
 
-    data = dataset.read()
+    asset_path = dataset.asset_path
+    with open(f"assets/objects/{dataset.asset_path}", 'rb') as file:
+        df_data = pd.read_csv(file)
+    # data_str = data.decode("utf-8")
+    # df_data = pd.read_csv(StringIO(data_str))
+    print('-' * 69)
+    print(df_data)
+    print('-' * 69)
     feature_list = []
-    for feature in data.columns:
-        if is_object_dtype(data[feature]):
+    for feature in df_data.columns:
+        if is_object_dtype(df_data[feature]):
             feature_type = "categorical"
-        elif is_numeric_dtype(data[feature]):
+        elif is_numeric_dtype(df_data[feature]):
             feature_type = "numerical"
         else:
             raise TypeError(f"{feature} has unsupported types")
