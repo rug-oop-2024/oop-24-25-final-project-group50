c452d319ca20983869938d3feedfef4c3cb39c08
Dan13l06 <167872814+Dan13l06@users.noreply.github.com>
2024-11-09 21:29:43 +0100

    Added pipeline saving

--- app/pages/2_⚙_Modelling.py
+++ app/pages/2_⚙_Modelling.py
@@ -3,6 +3,7 @@ import pickle
 import io
 
 from typing import Any
+from autoop.core.ml.model.model import Model
 from app.core.system import AutoMLSystem
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.model.regression import MultipleLinearRegression
@@ -16,26 +17,26 @@ from autoop.core.ml.model.classification.random_forest_classifier \
       import RandomForestClassifier
 from autoop.core.ml.pipeline import Pipeline
 from autoop.core.ml.artifact import Artifact
-
+from autoop.core.ml.feature import Feature
 from autoop.functional.feature import detect_feature_types
 from autoop.core.ml.metric import get_metric
 
 
-def get_model(name: str) -> Any:
-    match name:
-        case "Multiple linear regression":
-            model = MultipleLinearRegression()
-        case "Lasso":
-            model = LassoCV()
+def get_model(model_name: str) -> 'Model':
+    """Factory function to get a model by name."""
+    match model_name:
+        case "Multiple Linear Regression":
+            return MultipleLinearRegression()
         case "Elastic Net":
-            model = ElasticNet()
+            return ElasticNet()
+        case "Lasso":
+            return LassoCV()
         case "K Nearest Neighbors":
-            model = KNearestNeighbors()
-        case "Multilayer Preceptron":
-            model = MultiLayerPerceptron()
-        case "Random Forest Classifier":
-            model = RandomForestClassifier()
-    return model
+            return KNearestNeighbors()
+        case "Random ForestClassifier":
+            return RandomForestClassifier()
+        case "Multi Layer Perceptron":
+            return MultiLayerPerceptron()
 
 # METRICS = [
 #     "mean_squared_error",
@@ -86,7 +87,7 @@ target_feature = st.selectbox(label="Choose the target feature",
 
 if target_feature.type == "categorical":
     task_type = "classification"
-    model_options = ["K Nearest Neighbors", "Multilayer Preceptron",
+    model_options = ["K Nearest Neighbors", "Multi Layer Perceptron",
                      "Random Forest Classifier"]
     metric_options = [
         "Accuracy",
@@ -96,7 +97,7 @@ if target_feature.type == "categorical":
 
 else:
     task_type = "regression"
-    model_options = ["Multiple linear regression", "Lasso", "Elastic Net"]
+    model_options = ["Multiple Linear Regression", "Lasso", "Elastic Net"]
     metric_options = ["Mean Squared Error", "Mean Absolute Percentage Error",
                       "R-squared score"]
 
@@ -110,7 +111,6 @@ if name_cur_dataset and input_features and target_feature:
                            max_value=1.0, value=0.80)
     selected_metrics = st.multiselect(label="Select the metrics you \
                                        want to use:", options=metric_options)
-
 # Summary here
     if selected_metrics:
         names_input_features = [feature.name for feature in input_features]
@@ -119,7 +119,8 @@ if name_cur_dataset and input_features and target_feature:
         with col1:
             st.markdown(f"**Dataset**: {name_cur_dataset}")
             st.markdown(f"**Input Features**: {', '.join(names_input_features)
-                                               if input_features else 'None'}")
+                                               if input_features else 'None'}"
+                        )
             st.markdown(f"**Target**: {target_feature}")
 
         with col2:
@@ -170,55 +171,94 @@ if name_cur_dataset and input_features and target_feature:
 
             if pipeline_name and pipeline_version:
                 if st.button(label="Save pipeline"):
-                    cur_pipeline = st.session_state.cur_pipeline
-                    pipeline_artifacts = cur_pipeline.artifacts
+                    data = pickle.dumps({
+                     "dataset": name_cur_dataset,
+                     "model": model_selection,
+                     "input_features": input_features,
+                     "target_feature": target_feature,
+                     "metrics": selected_metrics,
+                     "split": data_split,
+                    })
 
-                    saved_pipeline = Artifact(
+                    pipeline_artifact = Artifact(
                         name=pipeline_name,
                         version=pipeline_version,
                         asset_path="./assets",
-                        type="pipeline",
-                        data=pickle.dumps("test")
-                    )
-                    for artifact in pipeline_artifacts:
-                        saved_pipeline.metadata[artifact.name] = artifact.id
-                        artifact.asset_path = f"./{artifact.id}"
-                        print(artifact)
-                        automl.registry.register(artifact)
+                        data=data,
+                        type='pipeline'
+                        )
+                    pipeline_artifact.asset_path = f"./{pipeline_artifact.id}"
+                    automl.registry.register(pipeline_artifact)
 
-                    saved_pipeline.asset_path = f"./{saved_pipeline.id}"
-                    automl.registry.register(saved_pipeline)
-                    st.success("Pipeline saved")
 
-                    def read_pickle_data(path):
-                        data_bytes = automl._storage.load(path)
-                        with io.BytesIO(data_bytes) as file:
-                            data = pickle.load(file)
-                        return data
+                    # pipeline_list = automl.registry.list(type="pipeline")
+                    # names_pipeline_list = [pipeline.name for pipeline in pipeline_list]
+                    # name_selected_pipeline = st.selectbox("Choose your saved pipeline", options=names_pipeline_list)
+
+
+                    # def artifact_to_pipeline(artifact: Artifact) -> "Pipeline":
+                    #     pipeline_dict = pickle.loads(artifact)
+                    #     return Pipeline(
+                    #         dataset=pipeline_dict.get("dataset"),
+                    #         model=pipeline_dict.get("model"),
+                    #         input_features=pipeline_dict.get("input_features"),
+                    #         target_feature=pipeline_dict.get("target_feature"),
+                    #         split=pipeline_dict.get("split")
+                    #     )
+
+                    # selected_pipeline = (pipeline_list[names_pipeline_list.index(name_selected_pipeline)])
+                    # new_pipeline = artifact_to_pipeline(selected_pipeline)
+
+                    # cur_pipeline = st.session_state.cur_pipeline
+                    # pipeline_artifacts = cur_pipeline.artifacts
+
+                    # saved_pipeline = Artifact(
+                    #     name=pipeline_name,
+                    #     version=pipeline_version,
+                    #     asset_path="./assets",
+                    #     type="pipeline",
+                    #     data=pickle.dumps("test")
+                    # )
+                    # for artifact in pipeline_artifacts:
+                    #     print("artifact", artifact)
+                    #     saved_pipeline.metadata[artifact.name] = artifact.id
+                    #     artifact.asset_path = f"./{artifact.id}"
+                    #     print(artifact)
+                    #     automl.registry.register(artifact)
+
+                    # saved_pipeline.asset_path = f"./{saved_pipeline.id}"
+                    # automl.registry.register(saved_pipeline)
+                    # st.success("Pipeline saved")
+
+                    # def read_pickle_data(path):
+                    #     data_bytes = automl._storage.load(path)
+                    #     with io.BytesIO(data_bytes) as file:
+                    #         data = pickle.load(file)
+                    #     return data
                     
-                    pipeline_list = automl.registry.list(type="pipeline")
-                    names_pipeline_list = [pipeline.name for pipeline in pipeline_list]
-                    data_pipeline_list = [pipeline.metadata for pipeline in pipeline_list]
-                    # print(names_pipeline_list)
-                    # print(data_pipeline_list)
-                    name_cur_pipeline = st.selectbox(label="Select the pipeline you \
-                                    want to use", options=names_pipeline_list)
+                    # pipeline_list = automl.registry.list(type="pipeline")
+                    # names_pipeline_list = [pipeline.name for pipeline in pipeline_list]
+                    # data_pipeline_list = [pipeline.metadata for pipeline in pipeline_list]
+                    # # print(names_pipeline_list)
+                    # # print(data_pipeline_list)
+                    # name_cur_pipeline = st.selectbox(label="Select the pipeline you \
+                    #                 want to use", options=names_pipeline_list)
 
-                    current_pipeline = pipeline_list[names_pipeline_list.index(name_cur_pipeline)]
+                    # current_pipeline = pipeline_list[names_pipeline_list.index(name_cur_pipeline)]
 
-                    config_path = current_pipeline.metadata.get("pipeline_config")
-                    pipeline_model = current_pipeline.metadata.get("pipeline_model_classification", "pipeline_model_regression")
-                    radius = current_pipeline.metadata.get("yield")
+                    # config_path = current_pipeline.metadata.get("pipeline_config")
+                    # pipeline_model = current_pipeline.metadata.get("pipeline_model_classification", "pipeline_model_regression")
+                    # radius = current_pipeline.metadata.get("yield")
 
-                    print("-"*70)
-                    print(read_pickle_data(radius))
-                    print(read_pickle_data(pipeline_model))
-                    print("-"*70)
+                    # print("-"*70)
+                    # print(read_pickle_data(radius))
+                    # print(read_pickle_data(pipeline_model))
+                    # print("-"*70)
 
-                    pipeline_config = read_pickle_data(config_path)
-                    input_features = pipeline_config.get('input_features')
-                    target_feature = pipeline_config.get("target_feature")
-                    split = pipeline_config.get("split")
+                    # pipeline_config = read_pickle_data(config_path)
+                    # input_features = pipeline_config.get('input_features')
+                    # target_feature = pipeline_config.get("target_feature")
+                    # split = pipeline_config.get("split")
 
 
         # return Pipeline(
@@ -257,3 +297,4 @@ if name_cur_dataset and input_features and target_feature:
 #                 automl.registry.register(artifact)
 #             automl.registry.register(saved_pipeline)
 #             st.success("Pipeline saved")
+
