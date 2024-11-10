91a63d6701e6f117f375d99502040be532cb39ee
njek02 <n.schepers@student.rug.nl>
2024-11-10 13:47:53 +0100

    Flake8 fix

--- autoop/core/ml/model/__init__.py
+++ autoop/core/ml/model/__init__.py
@@ -5,15 +5,15 @@ This package contains the get model function.
 
 from autoop.core.ml.model.model import Model
 from autoop.core.ml.model.regression.multiple_linear_regression \
- import MultipleLinearRegression
+     import MultipleLinearRegression
 from autoop.core.ml.model.regression.elastic_net import ElasticNet
 from autoop.core.ml.model.regression.lasso import LassoCV
 from autoop.core.ml.model.classification.random_forest_classifier \
- import RandomForestClassifier
+     import RandomForestClassifier
 from autoop.core.ml.model.classification.k_nearest_neighbours \
- import KNearestNeighbors
+     import KNearestNeighbors
 from autoop.core.ml.model.classification.multi_layer_perceptron \
- import MultiLayerPerceptron
+     import MultiLayerPerceptron
 
 REGRESSION_MODELS = [
     "Multiple Linear Regression",
